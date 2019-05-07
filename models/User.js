8a13cf8497b4ecd3851d9f26771fb1faeb66fb2c
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-05-07 19:44:13 -0400

    Created new schemas for the emergency contacts and who they are going on dates with

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// Create Schema
+// Create Schemas for user, who they are going on a date with and emergency contacts
 const UserSchema = new Schema({
   name: {
     type: String,
@@ -29,6 +29,42 @@ const UserSchema = new Schema({
   date: {
     type: Date,
     default: Date.now
+  },
+  dates: [DatesSchema],
+  contacts: [ContactsSchema]
+});
+
+const DatesSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  },
+  metThrough: {
+    type: String,
+    required: true
+  },
+  dateDescription: {
+    type: String,
+    required: true
+  }
+});
+
+const ContactsSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  },
+  relationship: {
+    type: String,
+    required: true
   }
 });
 module.exports = User = mongoose.model("users", UserSchema);
