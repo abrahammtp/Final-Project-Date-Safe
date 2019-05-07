82f5aa705150185b76cbddd62f6769806eabe6da
Francisco <fcruz219@gmail.com>
2019-05-07 14:23:17 -0400

    map is on page. need to work on database

--- client/src/components/MapsAPI/maps.js
+++ client/src/components/MapsAPI/maps.js
@@ -1,126 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
 
-const mapStyles = {
-  map: {
-    position: 'absolute',
-    width: '40%',
-    height: '60%'
-  }
-};
-export class CurrentLocation extends React.Component {
-  constructor(props) {
-    super(props);
+class Map extends Component {
 
-    const { lat, lng } = this.props.initialCenter;
-    this.state = {
-      currentLocation: {
-        lat: lat,
-        lng: lng
-      }
-    };
-  }
-  componentDidMount() {
-    if (this.props.centerAroundCurrentLocation) {
-      if (navigator && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(pos => {
-          const coords = pos.coords;
-          this.setState({
-            currentLocation: {
-              lat: coords.latitude,
-              lng: coords.longitude
-            }
-          });
-        });
-      }
+    componentDidMount() {
+        this.renderMap()
     }
-    this.loadMap();
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.google !== this.props.google) {
-      this.loadMap();
+    renderMap = () => {
+        loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCe9SUKM1ASyMMKdThjRFXBpPYGlFwTuz8&callback=initMap")
+        window.initMap = this.initMap
     }
-    if (prevState.currentLocation !== this.state.currentLocation) {
-      this.recenterMap();
+
+    initMap = () => {
+        var map = new window.google.maps.Map
+            (document.getElementById('map'), {
+                center: { lat: 28.548373, lng: -81.380369 },
+                zoom: 10
+            })
     }
-  }
 
-  loadMap() {
-    if (this.props && this.props.google) {
-      // checks if google is available
-      const { google } = this.props;
-      const maps = google.maps;
+    render() {
+        return (
+            <main>
+                <div id="map"></div>
+            </main>
+        )
 
-      const mapRef = this.refs.map;
-
-      // reference to the actual DOM element
-      const node = ReactDOM.findDOMNode(mapRef);
-
-      let { zoom } = this.props;
-      const { lat, lng } = this.state.currentLocation;
-      const center = new maps.LatLng(lat, lng);
-      const mapConfig = Object.assign(
-        {},
-        {
-          center: center,
-          zoom: zoom
-        }
-      );
-      // maps.Map() is constructor that instantiates the map
-      this.map = new maps.Map(node, mapConfig);
     }
-  }
-
-  recenterMap() {
-    const map = this.map;
-    const current = this.state.currentLocation;
-
-    const google = this.props.google;
-    const maps = google.maps;
-
-    if (map) {
-      let center = new maps.LatLng(current.lat, current.lng);
-      map.panTo(center);
-    }
-  }
-
-  renderChildren() {
-    const { children } = this.props;
-
-    if (!children) return;
-
-    return React.Children.map(children, c => {
-      if (!c) return;
-      return React.cloneElement(c, {
-        map: this.map,
-        google: this.props.google,
-        mapCenter: this.state.currentLocation
-      });
-    });
-  }
-
-  render() {
-    const style = Object.assign({}, mapStyles.map);
-
-    return (
-      <div className="map">
-        <div style={style} ref="map">
-          Loading map...
-        </div>
-        {this.renderChildren()}
-      </div>
-    );
-  }
 }
-export default CurrentLocation;
+/*
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCe9SUKM1ASyMMKdThjRFXBpPYGlFwTuz8&callback=initMap"
+async defer></script>
+*/
+function loadScript(url) {
+    var index = window.document.getElementsByTagName("script")[0]
 
-CurrentLocation.defaultProps = {
-  zoom: 14,
-  initialCenter: {
-    lat: -1.2884,
-    lng: 36.8233
-  },
-  centerAroundCurrentLocation: false,
-  visible: true
-};
+    var script = window.document.createElement("script")
+
+    script.src = url
+
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
+}
+export default Map
