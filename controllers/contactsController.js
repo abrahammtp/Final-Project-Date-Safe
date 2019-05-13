a26c94d454301df66cf9bf863c2a3aea62317250
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-05-13 13:03:22 -0400

    Worked on the contacts controllers and routes, they need to be tested

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,30 +2,26 @@ var db = require("../models");
 
 // Defining methods for ContactControllers
 module.exports = {
-    findAll: function (req, res) {
-        db.Contact.find(req.query)
-            .then(dbContact => res.json(dbContact))
-            .catch(err => res.status(422).json(err));
+    // Find a contact
+    find: function(req, res) {
+        db.Contact.find({ _nameId: req.params.id }).then(function(dbContact) {
+            res.json(dbContact);
+        });
     },
-    findById: function (req, res) {
-        db/Contact.findById(req.params.id)
-            .then(dbContact => res.json(dbContact))
-            .catch(err => res.status(422).json(err));
-    },
-    create: function (req, res) {
-        db.Contact.create(req.body)
-            .then(dbContact => res.json(dbContact))
-            .catch(err => res.status(422).json(err));
+    // Create a new contact
+    create: function(req, res) {
+        db.Contact.create(req.body).then(function(dbContact) {
+            res.json(dbContact);
+        });
     },
     update: function (req, res) {
-        db.Contact.findOneAndUpdate({ id: req.params.id }, req, body)
+        db.Contact.findOneAndUpdate({ _id: req.params.id }, req, body)
             .then(dbContact => res.json(dbContact))
             .catch(err => res.status(422).json(err));
     },
-    remove: function (req, res) {
-        db.Contact.findById(req.params.id)
-            .then(dbContact => dbContact.remove())
-            .then(dbContact => res.json(dbContact))
-            .catch(err => res.status(422).json(err));
+    remove: function(req, res) {
+        db.Contact.remove({ _id: req.params.id }).then(function(dbContact) {
+            res.json(dbContact);
+        });
     }
 };
