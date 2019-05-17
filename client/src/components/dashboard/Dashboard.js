3e06c0ac9302d8a1a66a8f74d1b6789d75faf727
Francisco <fcruz219@gmail.com>
2019-05-17 15:49:16 -0400

    GET sort of working. needs to be dynamic

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,8 +7,29 @@ import { Row, Container, Col } from 'react-grid-system';
 import "./style.css"
 import Contacts from "./Contacts";
 import { Link } from "react-router-dom";
+import api from "../../utils/api"
+
 
 class Dashboard extends Component {
+  state = {
+    dates: [],
+    dateName: "",
+    message: "test"
+  };
+  getSavedDates = () => {
+    api.getSavedDates(this.state.q)
+    .then(res =>
+      this.setState({
+        dates: res.data
+      })
+    );
+} 
+
+handleFormSubmit = event => {
+  event.preventDefault();
+  this.getSavedDates();
+};
+  
   render() {
     const { user } = this.props.auth;
 
@@ -29,6 +50,7 @@ class Dashboard extends Component {
                 <Link to="/dates">
                   <button className="btn waves-effect" type="submit" name="action">New Date</button>
                 </Link>
+                  <button className="btn waves-effect" type="submit" name="action" onClick={() => this.getSavedDates()}>Refresh Dates</button>
               </Container>
             </Col>
             <Col>
@@ -63,18 +85,18 @@ class Dashboard extends Component {
               <Container className="userContainer">
                 <Col>
                   <Col>
-                    <h5 className="dateOne">Upcoming dates</h5>
-                    <p className="dateOne1">Date on 05/22/2019</p>
-                    <p className="date1">Going to Barcade with Kelly</p>
+                    <h5 className="dateOne">Upcoming date</h5>
+                    <p className="dateOne1">Date on {user.dateWhen}</p>
+                    <p className="date1">Going to {user.dateAddress} with {user.dateName}</p>
                   </Col>
                   <Col>
-                    <h5>Previous dates</h5>
+                    {/* <h5>Previous dates</h5>
                     <p>Date's Name: {user.dateName}</p>
                     <p>Date's Phone Number: {user.dateNumber}</p>
                     <p>How you met: {user.metThrough}</p>
                     <p>Description of Date: {user.dateDescription}</p>
                     <p>Where you're going: {user.dateAddress}</p>
-                    <p>When the date is: {user.dateWhen}</p>
+                    <p>When the date is: {user.dateWhen}</p> */}
                   </Col>
                 </Col>
               </Container>
